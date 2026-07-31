8b0d3cae6b603b66192ac8b811ec1acde807b1bb
Paweł Potępa <potepapawel@gmail.com>
2026-07-31 23:08:37 +0200

    Add Android notification navigation contract

---
+++ mobile/android/app/src/main/kotlin/org/torchat/mobile/NotificationNavigation.kt
@@ -0,0 +1,8 @@
+package org.torchat.mobile
+
+internal object NotificationNavigation {
+    const val ACTION_OPEN_CONVERSATION = "org.torchat.mobile.OPEN_CONVERSATION"
+    const val EXTRA_CONVERSATION_ID = "conversationId"
+    const val EXTRA_NOTIFICATION_ID = "notificationId"
+    const val EVENT_TYPE = "notification_opened"
+}
