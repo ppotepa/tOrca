e0a0f35332177b0039a5093013cc10902a33dcc0
Paweł Potępa <potepapawel@gmail.com>
2026-07-31 21:10:58 +0200

    Export relationship tombstone storage

--- common/torchat-client-engine/src/storage/mod.rs
+++ common/torchat-client-engine/src/storage/mod.rs
@@ -1,15 +1,16 @@
 pub mod migrations;
+pub mod relationship;
 pub mod runtime_storage;
 pub mod sqlite;
 pub mod transaction;
 
 pub use migrations::{Migration, MigrationRunner};
+pub use relationship::RelationshipTombstone;
 pub use runtime_storage::SqliteRuntimeStorage;
 pub use sqlite::{
     ClientDatabase, DeliveryReceiptRecord, InboundEnvelopeStoreResult, InboundPeerEnvelopeRecord,
     OutboundDeliveryRecord, PairingResponseRecord, PeerEndpointBootstrapRecord,
     PendingContactConfirmationRecord, PendingPeerEndpointInboxRecord, PendingWelcomeRecord,
-    ReceivedEnvelopeRecord,
-    RetryDeadline, RetryKind, StoredMessageRecord,
+    ReceivedEnvelopeRecord, RetryDeadline, RetryKind, StoredMessageRecord,
 };
 pub use transaction::SqliteTransaction;
