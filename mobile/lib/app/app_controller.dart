c380e0032c145bcfa4e408d8b9ced937b65215e2
Paweł Potępa <potepapawel@gmail.com>
2026-07-31 21:03:17 +0200

    Initialize desktop window lifecycle with app controller

--- mobile/lib/app/app_controller.dart
+++ mobile/lib/app/app_controller.dart
@@ -1,11 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app_controller_legacy.dart' as legacy;
+import 'desktop_window_lifecycle.dart';
 import 'notification_safe_app_controller.dart';
 
 export 'app_controller_legacy.dart' hide appControllerProvider;
 
 final appControllerProvider =
-    NotifierProvider<legacy.AppController, legacy.AppState>(
-  () => NotificationSafeAppController(),
-);
+    NotifierProvider<legacy.AppController, legacy.AppState>(() {
+  unawaited(DesktopWindowLifecycle.initialize());
+  return NotificationSafeAppController();
+});
