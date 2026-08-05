820a7aafb482cd8a5217cd48c39e5f795dbef3d7
Paweł Potępa <potepapawel@gmail.com>
2026-08-05 11:23:21 +0200

    build(engine): allow staged contract helpers

--- packages/torchat-client-engine/src/lib.rs
+++ packages/torchat-client-engine/src/lib.rs
@@ -6,13 +6,16 @@ pub mod error;
 pub mod event;
 mod effects;
 pub mod fault_injection;
+#[allow(dead_code)]
 mod input;
 mod logging;
 mod output;
 pub mod peer;
+#[allow(dead_code)]
 mod processing;
 pub mod probing;
 pub mod relay;
+#[allow(dead_code)]
 mod scheduler;
 pub mod storage;
 
