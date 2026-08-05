3700bae44bc9fb9e5470902432c1caab910ea022
Paweł Potępa <potepapawel@gmail.com>
2026-08-05 14:05:42 +0200

    refactor(engine): keep output publisher private

--- packages/torchat-client-engine/src/output/mod.rs
+++ packages/torchat-client-engine/src/output/mod.rs
@@ -3,5 +3,4 @@ mod publisher;
 mod response_registry;
 
 pub(crate) use event_router::spawn_event_router;
-pub(crate) use publisher::spawn_public_event_publisher;
 pub(crate) use response_registry::PendingResponseRegistry;
