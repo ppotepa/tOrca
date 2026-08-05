b019c8a767db6703058899acf1f4d8ff05b9d85a
Paweł Potępa <potepapawel@gmail.com>
2026-08-05 12:32:34 +0200

    refactor(engine): extract public event publisher

---
+++ packages/torchat-client-engine/src/output/publisher.rs
@@ -0,0 +1,16 @@
+use tokio::sync::mpsc;
+
+use crate::EngineEvent;
+
+pub(super) fn spawn_public_event_publisher(
+    mut publish_rx: mpsc::UnboundedReceiver<EngineEvent>,
+    public_events: mpsc::Sender<EngineEvent>,
+) {
+    tokio::spawn(async move {
+        while let Some(event) = publish_rx.recv().await {
+            if public_events.send(event).await.is_err() {
+                break;
+            }
+        }
+    });
+}
