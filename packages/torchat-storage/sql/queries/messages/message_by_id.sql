893bb4ab15f41a06d14fbb2f7cb1cfcbed369de7
Paweł Potępa <potepapawel@gmail.com>
2026-08-05 23:12:35 +0200

    refactor(storage): add direct message lookup

---
+++ packages/torchat-storage/sql/queries/messages/message_by_id.sql
@@ -0,0 +1,6 @@
+SELECT id, conversation_id, outgoing, body, reply_to_json, state, created_at,
+       attempt_count, last_attempt_at, next_attempt_at, ack_deadline,
+       last_transport_error
+FROM messages
+WHERE id = ?1
+LIMIT 1;
