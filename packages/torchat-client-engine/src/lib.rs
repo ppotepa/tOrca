aa8c06c201011c4c5c617faa251ba61ff375768e
Paweł Potępa <potepapawel@gmail.com>
2026-08-05 10:57:41 +0200

    refactor(engine): register unified scheduler

--- packages/torchat-client-engine/src/lib.rs
+++ packages/torchat-client-engine/src/lib.rs
@@ -5,14 +5,13 @@ pub mod engine;
 pub mod error;
 pub mod event;
 pub mod fault_injection;
-#[allow(dead_code)]
 mod input;
 mod logging;
 pub mod peer;
-#[allow(dead_code)]
 mod processing;
 pub mod probing;
 pub mod relay;
+mod scheduler;
 pub mod storage;
 
 pub use actor::ClientEngineActor;
