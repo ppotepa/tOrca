51909078449ef9ba0cfffd16507b59993fcf2523
Paweł Potępa <potepapawel@gmail.com>
2026-08-05 12:10:43 +0200

    refactor(engine): split command contract modules

---
+++ packages/torchat-client-engine/src/contract/mod.rs
@@ -0,0 +1,11 @@
+mod command;
+mod command_envelope;
+mod platform_fact;
+mod platform_kind;
+mod tor_phase;
+
+pub use command::EngineCommand;
+pub use command_envelope::EngineCommandEnvelope;
+pub use platform_fact::PlatformFact;
+pub use platform_kind::PlatformKind;
+pub use tor_phase::TorPhase;
